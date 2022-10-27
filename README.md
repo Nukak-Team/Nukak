01f63c58e2e81424e9fa52fbea51f00086ded377
AlexSerrano121 <alexander10139@gmail.com>
2022-10-27 07:06:52 -0500

    Se inicializa el proyecto

--- README.md
+++
@@ -1,5 +0,0 @@
-# Nukak
-Modelo de sistema de información que soporte la gestión de una empresa que vende herramientas
-
-#Ruben Alexander Serrano
-Se realliza commit para cumplir con el primer sprint.
---
+++ ferreteria_nukak/src/components/listaProductos/listaProductos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './listaProductos.module.css';
+
+const ListaProductos = () => (
+  <div className={styles.ListaProductos}>
+    ListaProductos Component
+  </div>
+);
+
+ListaProductos.propTypes = {};
+
+ListaProductos.defaultProps = {};
+
+export default ListaProductos;
